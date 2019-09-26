22ebe9c9b1b56b37d7fc23fe153e4b24c6ce0046
KhanhNB <khanh.nguyenba.97bn@gmail.com>
2019-09-26 10:07:41 +0700

    KhanhNB commit

--- src/main/java/vn/topica/itlab4/springweb/controller/UserController.java
+++ src/main/java/vn/topica/itlab4/springweb/controller/UserController.java
@@ -1,10 +1,14 @@
 package vn.topica.itlab4.springweb.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import vn.topica.itlab4.springweb.model.UserModel;
@@ -24,13 +28,15 @@ public class UserController {
 	}
 
 	@PostMapping("/user/login")
-	public ModelAndView processLogin(@ModelAttribute("user") UserModel userModel) {
+	public ModelAndView processLogin(@ModelAttribute("user") UserModel userModel, HttpServletRequest request) {
 
 		ModelAndView modelAndView = new ModelAndView();
 		UserModel user = userService.findUserByUserName(userModel.getUserName());
 
 		if (user != null) {
 			if (userModel.getPassword().equals(user.getPassword())) {
+				HttpSession session = request.getSession();
+				session.setAttribute("userId", user.getId());
 				modelAndView.setViewName("/home/index");
 				return modelAndView;
 			} else {
@@ -44,6 +50,31 @@ public class UserController {
 			modelAndView.setViewName("login");
 			return modelAndView;
 		}
+	}
+	
+	public ModelAndView logout(HttpServletRequest request) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/login");
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/*")
+	public ModelAndView doLogin(HttpServletRequest request) {
+		System.out.println("/////////////////////");
+		HttpSession session = request.getSession();
+		
+		Integer userId = (Integer) session.getAttribute("userId");
+		
+		if(userId == null) {
+			return new ModelAndView("/user/login");
+		} else {
+			return new ModelAndView("/home/index");
+		}
+		
+		
 	}
 
 }
